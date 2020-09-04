4e6ec6d3fc5a564a2cf65a52e4b3e2f93cc7b136
PhamBaoNgoc <59905079+PhamBaoNgoc@users.noreply.github.com>
2020-09-04 20:48:17 +0700

    Add project files.

---
+++ Practical-UWP/Models/Contacts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_UWP.Models
+{
+    public class Contacts
+    {
+        public string Phone { get; set; }
+        public string Name { get; set; }
+
+    }
+}
